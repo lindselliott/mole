399a6ac0b7790cc8e3e0db2bf8590a1d6f3944a0
Lindsay Elliott <lellio04@uoguelph.ca>
2019-01-26 18:48:14 -0500

    making progress

---
+++ app/src/main/java/com/example/lindsay/delta5/screens/ProfileFragment.kt
@@ -0,0 +1,49 @@
+package com.example.lindsay.delta5.screens
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_profile.*
+
+class ProfileFragment : Fragment() {
+    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    private fun setAllFields() {
+        //Age
+        age_text.text = "22"
+
+        // Sex
+        var adapter = ArrayAdapter.createFromResource(this, R.array.sex_type, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        sex_spinner.adapter = adapter
+    }
+
+}
