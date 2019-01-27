eb8cb0e0062820150ab712e8e6de5394031ec615
Marshall Asch <maasch@rogers.com>
2019-01-26 20:53:57 -0500

    Added a basic model of the mole

---
+++ app/src/main/java/com/example/lindsay/delta5/models/MoleModel.kt
@@ -0,0 +1,74 @@
+package com.example.lindsay.delta5.models
+
+import android.util.Log
+import com.example.lindsay.delta5.entities.Mole
+import com.example.lindsay.delta5.utils.ImageUtils
+import io.realm.Realm
+import io.realm.RealmResults
+
+/**
+ *
+ * @author Marshall Asch
+ * @version 1.0
+ * @since 2019-01-26
+ */
+class MoleModel {
+    companion object {
+        fun removePlant(realm: Realm, mole: Mole): Boolean {
+            if (mole.imagePath != null) {
+                ImageUtils.deleteImage(mole.imagePath!!)
+            }
+
+            try {
+                realm.beginTransaction()
+                mole.deleteFromRealm()
+                realm.commitTransaction()
+            } catch (e: Exception) {
+                Log.e("MoleModel", e.message)
+                if (realm.isInTransaction) {
+                    realm.cancelTransaction()
+                }
+
+                return false
+            }
+
+            return true
+        }
+
+        fun savePlant(realm: Realm, mole: Mole): Boolean {
+            try {
+                realm.beginTransaction()
+                realm.insertOrUpdate(mole)
+                realm.commitTransaction()
+            } catch (e: Exception) {
+                Log.e("MoleModel", e.message)
+                if (realm.isInTransaction) {
+                    realm.cancelTransaction()
+                }
+
+                return false
+            }
+            return true
+        }
+
+
+        fun getAllMoles(realm: Realm): RealmResults<Mole> {
+            return realm.where(Mole::class.java).findAll()
+        }
+
+        fun removeAllMoles(realm: Realm): Boolean {
+            try {
+                realm.beginTransaction()
+                realm.delete(Mole::class.java)
+                realm.commitTransaction()
+            } catch (e: java.lang.Exception) {
+                if (realm.isInTransaction) {
+                    realm.cancelTransaction()
+                }
+                return false
+            }
+
+            return true
+        }
+    }
+}
