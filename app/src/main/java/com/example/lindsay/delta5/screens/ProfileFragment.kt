90092e83f8b50a2dd4c2e1585dce9d29815afeaa
Lindsay Elliott <lellio04@uoguelph.ca>
2019-01-26 20:36:32 -0500

    saving user

--- app/src/main/java/com/example/lindsay/delta5/screens/ProfileFragment.kt
+++ app/src/main/java/com/example/lindsay/delta5/screens/ProfileFragment.kt
@@ -1,17 +1,22 @@
 package com.example.lindsay.delta5.screens
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.lindsay.delta5.Application
 import kotlinx.android.synthetic.main.fragment_profile.*
 
 import com.example.lindsay.delta5.R
+import com.example.lindsay.delta5.entities.User
+import com.example.lindsay.delta5.models.UserModel
 
 class ProfileFragment : Fragment() {
+    private lateinit var application: Application
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -45,6 +50,20 @@ class ProfileFragment : Fragment() {
         var adapter = ArrayAdapter.createFromResource(this.context, R.array.sex_type, android.R.layout.simple_spinner_item)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         sex_spinner.adapter = adapter
+    }
+
+    private fun saveUser(isEdit: Boolean): Boolean {
+        val age = if (age_text.text.toString().isBlank()) "" else  age_text.text.toString()
+        val sex = sex_spinner.selectedItemPosition
+
+
+        val userToSave = User(
+                "1",
+                age.toInt(),
+                sex
+        )
+
+        return UserModel.saveUser(application.getRealm(), userToSave)
     }
 
 
